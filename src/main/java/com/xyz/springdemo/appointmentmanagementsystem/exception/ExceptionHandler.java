8910815a50e00fe9d93690f6eef7133906ca2e70
arvndx <iamarvind006@gmail.com>
2021-12-08 10:32:51 +0530

    fixed code smells

--- src/main/java/com/xyz/springdemo/appointmentmanagementsystem/exception/ExceptionHandler.java
+++ src/main/java/com/xyz/springdemo/appointmentmanagementsystem/exception/ExceptionHandler.java
@@ -8,39 +8,38 @@ import java.text.ParseException;
 @ControllerAdvice
 public class ExceptionHandler {
 
-    private final static  String urlForError = "error/error-page";
-    private final static String message = "message";
+    private static final String ERROR_PAGE = "error/error-page";
+    private static final String MESSAGE = "message";
+
     @org.springframework.web.bind.annotation.ExceptionHandler
     public String parseException(ParseException e,Model model){
-        String msg = "please enter the date and time in correct format";
-        model.addAttribute(message,msg);
-        return urlForError;
+        model.addAttribute(MESSAGE,e.getMessage());
+        return ERROR_PAGE;
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler
     public String patientNotFoundExceptionHandler(PatientNotFoundException e, Model model) {
-        model.addAttribute(message, e.getMessage());
-        return urlForError;
+        model.addAttribute(MESSAGE, e.getMessage());
+        return ERROR_PAGE;
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler
     public String doctorNotFoundExceptionHandler(DoctorNotFoundException e, Model model) {
-        model.addAttribute(message, e.getMessage());
-        return urlForError;
+        model.addAttribute(MESSAGE, e.getMessage());
+        return ERROR_PAGE;
     }
-
 
     @org.springframework.web.bind.annotation.ExceptionHandler
     public String exceptionHandler(MyRuntimeException e, Model model) {
-        model.addAttribute(message, e.getMessage());
-        return urlForError;
+        model.addAttribute(MESSAGE, e.getMessage());
+        return ERROR_PAGE;
     }
 
 
     @org.springframework.web.bind.annotation.ExceptionHandler
     public String exceptionHandler(Exception e, Model model){
-        model.addAttribute(message,e.getMessage());
-        return urlForError;
+        model.addAttribute(MESSAGE,e.getMessage());
+        return ERROR_PAGE;
     }
 
 
